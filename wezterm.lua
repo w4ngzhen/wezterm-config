096fe5583d06fd502ded49071fe46c4fe096f39b
zhen <i@zhen.wang>
2026-01-15 09:20:15 +0800

    modify config

--- wezterm.lua
+++ wezterm.lua
@@ -16,7 +16,7 @@ if use_default_prog then
         -- todo
     elseif is_mac_os then
         -- https://github.com/zellij-org/zellij-org.github.io/issues/284
-        config.default_prog = { "zsh", "-c", "zellij -l welcome" }
+        config.default_prog = { "zsh", "-l", "-c", "zellij -l welcome" }
     end
 end
 
@@ -26,17 +26,17 @@ local act = wezterm.action;
 -- 禁用所有默认的按键配置，仅使用下面的按键配置
 config.disable_default_key_bindings = true;
 config.keys = {
-  { key = 'w',          mods = 'ALT',        action = act.ShowLauncher },
-  { key = "RightArrow", mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab },
-  { key = 'LeftArrow',  mods = 'SHIFT',      action = act.ActivateTabRelative(-1) },
-  { key = 'RightArrow', mods = 'SHIFT',      action = act.ActivateTabRelative(1) },
+    { key = 'w',          mods = 'ALT',        action = act.ShowLauncher },
+    { key = "RightArrow", mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab },
+    { key = 'LeftArrow',  mods = 'SHIFT',      action = act.ActivateTabRelative(-1) },
+    { key = 'RightArrow', mods = 'SHIFT',      action = act.ActivateTabRelative(1) },
 }
 
 -- macOS command特殊按键绑定
 if is_mac_os then
     table.insert(config.keys, { key = "c", mods = "CMD", action = act.CopyTo 'Clipboard' })
     table.insert(config.keys, { key = "v", mods = "CMD", action = act.PasteFrom 'Clipboard' })
-    table.insert(config.keys, { key = "n", mods = "CMD", action = act.SpawnCommandInNewTab { } })
+    table.insert(config.keys, { key = "n", mods = "CMD", action = act.SpawnCommandInNewTab {} })
     table.insert(config.keys, { key = "w", mods = "CMD", action = act.CloseCurrentTab { confirm = true } })
 end
 
@@ -45,21 +45,21 @@ end
 config.launch_menu = {}
 -- Using shell
 if is_windows then
-  -- Windows
-  table.insert(config.launch_menu, {
-    label = 'PowerShell-Tab',
-    args = { "pwsh.exe" }
-  })
-  table.insert(config.launch_menu, {
-    label = 'CMD-Tab',
-    args = { 'cmd.exe' }
-  })
+    -- Windows
+    table.insert(config.launch_menu, {
+        label = 'PowerShell-Tab',
+        args = { "pwsh.exe" }
+    })
+    table.insert(config.launch_menu, {
+        label = 'CMD-Tab',
+        args = { 'cmd.exe' }
+    })
 elseif is_mac_os then
-  -- macOS (Intel)
-  table.insert(config.launch_menu, {
-    label = 'zsn-Tab',
-    args = { 'zsh', '-l' }
-  })
+    -- macOS (Intel)
+    table.insert(config.launch_menu, {
+        label = 'zsn-Tab',
+        args = { 'zsh', '-l' }
+    })
 end
 
 -- CONFIG: font
@@ -67,7 +67,7 @@ end
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 -- font config
 config.font = wezterm.font_with_fallback {
-  'JetBrainsMono Nerd Font'
+    'JetBrainsMono Nerd Font'
 };
 config.font_size = 18;
 config.line_height = 1.2;
