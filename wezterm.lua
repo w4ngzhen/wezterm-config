52234ad937d2c292b92d16e2e3ff35f1be74d1f8
zhen <i@zhen.wang>
2026-01-15 08:26:03 +0800

    update config

--- wezterm.lua
+++ wezterm.lua
@@ -2,58 +2,67 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 500 }
+-- windows
+local is_windows = wezterm.target_triple == 'x86_64-pc-windows-msvc';
+-- macOS (Apple Silicon or Intel CPU)
+local is_mac_os = wezterm.target_triple == 'aarch64-apple-darwin' or wezterm.target_triple == 'x86_64-apple-darwin';
 
--- 启动menu
+-- CONFIG: default prog
+-- 请注意，使用default_prog请确保相关工具已经安装
+local use_default_prog = true;
+
+if use_default_prog then
+    if is_windows then
+        -- todo
+    elseif is_mac_os then
+        -- https://github.com/zellij-org/zellij-org.github.io/issues/284
+        config.default_prog = { "zsh", "-c", "zellij -l welcome" }
+    end
+end
+
 local act = wezterm.action;
-
--- keyboard mappings
+-- CONFIG: keyboard mappings
+-- https://wezterm.org/config/lua/keyassignment/index.html
 -- 禁用所有默认的按键配置，仅使用下面的按键配置
 config.disable_default_key_bindings = true;
 config.keys = {
   { key = 'w',          mods = 'ALT',        action = act.ShowLauncher },
-  --
-  { key = 'LeftArrow',  mods = 'CTRL',        action = act.ActivatePaneDirection 'Left' },
-  { key = 'RightArrow', mods = 'CTRL',        action = act.ActivatePaneDirection 'Right' },
-  { key = 'UpArrow',    mods = 'CTRL',        action = act.ActivatePaneDirection 'Up' },
-  { key = 'DownArrow',  mods = 'CTRL',        action = act.ActivatePaneDirection 'Down' },
-  --
-  { key = 'RightArrow', mods = 'SHIFT',      action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = 'DownArrow',  mods = 'SHIFT',      action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-  { key = 'p',          mods = 'CTRL|SHIFT', action = act.ActivateCommandPalette },
-  -- macOS only
-  { key = 'w',          mods = 'CMD', action = wezterm.action.CloseCurrentTab { confirm = true }, },
-  { key = 'n',          mods = 'CMD', action = wezterm.action.SpawnCommandInNewTab { }, },
+  { key = "RightArrow", mods = "CTRL|SHIFT", action = act.SpawnCommandInNewTab },
+  { key = 'LeftArrow',  mods = 'SHIFT',      action = act.ActivateTabRelative(-1) },
+  { key = 'RightArrow', mods = 'SHIFT',      action = act.ActivateTabRelative(1) },
 }
+
+-- macOS command特殊按键绑定
+if is_mac_os then
+    table.insert(config.keys, { key = "c", mods = "CMD", action = act.CopyTo 'Clipboard' })
+    table.insert(config.keys, { key = "v", mods = "CMD", action = act.PasteFrom 'Clipboard' })
+    table.insert(config.keys, { key = "n", mods = "CMD", action = act.SpawnCommandInNewTab { } })
+    table.insert(config.keys, { key = "w", mods = "CMD", action = act.CloseCurrentTab { confirm = true } })
+end
+
+
+-- CONFIG: launch_menu
 config.launch_menu = {}
 -- Using shell
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+if is_windows then
   -- Windows
   table.insert(config.launch_menu, {
-    label = 'PowerShell-NewWindow',
+    label = 'PowerShell-Tab',
     args = { "pwsh.exe" }
   })
   table.insert(config.launch_menu, {
-    label = 'CMD-NewWindow',
+    label = 'CMD-Tab',
     args = { 'cmd.exe' }
   })
-elseif wezterm.target_triple == 'x86_64-apple-darwin' then
+elseif is_mac_os then
   -- macOS (Intel)
   table.insert(config.launch_menu, {
-    label = 'zsn-NewWindow',
+    label = 'zsn-Tab',
     args = { 'zsh', '-l' }
   })
-  table.insert(config.keys, { key = "c", mods = "CMD", action = act.CopyTo 'Clipboard' })
-  table.insert(config.keys, { key = "v", mods = "CMD", action = act.PasteFrom 'Clipboard' })
-elseif wezterm.target_triple == 'aarch64-apple-darwin' then
-  -- macOS (Apple Silicon)
-  table.insert(config.launch_menu, {
-    label = 'zsn-NewWindow',
-    args = { 'zsh', '-l' }
-  })
-  table.insert(config.keys, { key = "c", mods = "CMD", action = act.CopyTo 'Clipboard' })
-  table.insert(config.keys, { key = "v", mods = "CMD", action = act.PasteFrom 'Clipboard' })
 end
+
+-- CONFIG: font
 -- disable Font Shaping
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 -- font config
@@ -62,8 +71,8 @@ config.font = wezterm.font_with_fallback {
 };
 config.font_size = 18;
 config.line_height = 1.2;
---
--- theme
+
+-- CONFIG: theme
 config.color_scheme = "JetBrains Darcula"
 --
 -- all config
